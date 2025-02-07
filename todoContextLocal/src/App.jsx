eb2f4b833b21b6c3d62536c9051a869ac5394152
Madhur Chauhan <madhurchauhan1100@gmail.com>
2025-02-07 18:52:11 +0530

    To do App

---
+++ todoContextLocal/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="text-3xl text-center justify-center items-center">Todo App with context api</div>
+    </>
+  )
+}
+
+export default App
